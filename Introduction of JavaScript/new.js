71eb96629298fc85fc030304386a379b9442493a
saga-r-code <135031980+saga-r-code@users.noreply.github.com>
2024-03-13 02:16:40 +0530

    New Language Add

---
+++ Introduction of JavaScript/new.js
@@ -0,0 +1,5 @@
+console.log("Hello World")
+console.log("Code is running...")
+console.log("Code is also running...")
+console.log("Code is looking like a wow...")
+console.log(10 + 5) // adding two numbers in JavaScript

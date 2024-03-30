47b320ea15f2bda878da22df605f375c6a917b3b
saga-r-code <135031980+saga-r-code@users.noreply.github.com>
2024-03-30 22:22:55 +0530

    Update script.js

--- Factorial/script.js
+++ Factorial/script.js
@@ -1,11 +1,24 @@
-let arr = [];
+// let arr = [];
 
-for (i = 1; i <= 5; i++) {
-  arr.push(i);
+// for (i = 1; i <= 5; i++) {
+//   arr.push(i);
+// }
+// console.log(arr);
+
+// let n = arr.reduce((a, b) => {
+//   return a * b;
+// });
+// console.log("Factorial of 5 number is : " + n);
+
+let num = 5;
+
+function factorial(number) {
+  let arr = Array.from(Array(number + 1).keys());
+  console.log(arr.slice(1));
+  let fact = arr.slice(1).reduce((a, b) => {
+    return a * b;
+  });
+  console.log(fact);
 }
-console.log(arr);
 
-let n = arr.reduce((a, b) => {
-  return a * b;
-});
-console.log("Factorial of 5 number is : " + n);
+factorial(num);

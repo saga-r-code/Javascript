b76336175d2dc6401fe4df9a8b6289862bcb673d
saga-r-code <135031980+saga-r-code@users.noreply.github.com>
2024-04-20 17:39:26 +0530

    Add files via upload

---
+++ DOM And Related Projects/DOM INTRO/DOM_WithShreyanshCoding.js
@@ -0,0 +1,15 @@
+var s = document.querySelector("h1")//.innerHTML  = "SAGAR"
+ s.innerHTML = "HEY";
+
+
+s.style.color = "red";
+s.style.backgroundColor = "black"
+ console.log(s);
+
+ s.addEventListener("click", function(){
+   s.innerHTML = "Dekh Kya Raha Hai Mc";
+   s.style.color = "White";
+ });
+
+
+
